6b13aeb61eea4d5aa263359cb06786f57d81b7ba
choc1024 <gamedeveloper1024@outlook.com>
2026-01-19 09:57:14 +0000

    storage functions and timer page basic layout

---
+++ storage.js
@@ -0,0 +1,31 @@
+if (localStorage.getItem("first-time") == null) {
+    const times_preset = {
+        sessions: {
+
+        }
+    };
+
+    const settings_preset = {
+        timer: {
+            wca_inspection: false,
+            focus_mode: false,
+            hide_time: false
+        }
+    }
+
+    localStorage.setItem("timer_data", JSON.stringify(times_preset))
+    localStorage.setItem("settings", JSON.stringify(settings_preset))
+    localStorage.setItem("first-time", "no")
+}
+
+
+function get_data(key) {
+    const raw_data = localStorage.getItem(key);
+    const json_data = raw_data ? JSON.parse(raw_data) : null;
+
+    return json_data
+}
+
+function set_data(key, json_data) {
+    localStorage.setItem(key, JSON.stringify(json_data));
+}
---
+++ timer.js
@@ -0,0 +1,5 @@
+const main_page = window.parent
+
+const timer_settings = main_page.get_data("settings").timer
+
+console.log(timer_settings)
