941c89243a5c75f38bd7f11bd2778b0e514c28bd
choc1024 <gamedeveloper1024@outlook.com>
2026-01-19 17:26:14 +0000

    timer prototype

--- timer.js
+++ timer.js
@@ -3,3 +3,36 @@ const main_page = window.parent
 const timer_settings = main_page.get_data("settings").timer
 
 console.log(timer_settings)
+
+
+const timer = document.getElementById("timer-counter")
+
+let spaceDownTime = null;
+let spaceHeld = false;
+
+let inspecting = false
+
+document.addEventListener("keydown", (e) => {
+    if (e.code === "Space" && !spaceHeld) {
+        spaceHeld = true;
+        spaceDownTime = performance.now();
+        setTimeout(() => {
+            timer.style.color = "green"
+        }, 1000);
+
+    }
+});
+
+document.addEventListener("keyup", (e) => {
+    if (e.code === "Space") {
+        const heldTime = performance.now() - spaceDownTime;
+        spaceHeld = false;
+
+        if (heldTime >= 1000) {
+            if (timer_settings.wca_inspection === true && inspecting === false) {
+                inspecting = true
+
+            }
+        }
+    }
+});
