a7a09706ece8612e8a33149d07ad37bee69e90db
choc1024 <gamedeveloper1024@outlook.com>
2026-01-21 11:44:11 +0000

    basic timer logic

--- timer.js
+++ timer.js
@@ -13,12 +13,21 @@ let spaceHeld = false;
 
 let inspecting = false
 
+let timing = false
 
 let holdTimeout = null;
 
 document.addEventListener("keydown", (e) => {
     if (e.code === "Space" && !spaceHeld) {
         e.preventDefault();
+        if (inspecting) {
+          inspecting = false;
+          timing = true;
+        }
+
+        if (timing) {
+          timing = false;
+        }
         spaceHeld = true;
         spaceDownTime = performance.now();
 
@@ -34,5 +43,47 @@ document.addEventListener("keyup", (e) => {
         holdTimeout = null;
         spaceHeld = false;
         timer.style.color = "white";
+
+        if (performance.now() - spaceDownTime >= 1000) {
+            if (timer_settings.wca_inspection && !timing) {
+                inspecting = true
+                startInspection();
+            } else {
+                timing = true
+                startTiming();
+            }
+        }
     }
 });
+
+
+
+function startInspection() {
+  timer.textContent = "0.000";
+  const start = performance.now();
+
+  const interval = setInterval(() => {
+    if (!inspecting) {
+      clearInterval(interval);
+      return;
+    }
+
+    const elapsed = performance.now() - start;
+    timer.textContent = (elapsed / 1000).toFixed(3);
+  }, 1);
+}
+
+function startTiming() {
+  timer.textContent = "0.000";
+  const start = performance.now();
+
+  const interval = setInterval(() => {
+    if (!timing) {
+      clearInterval(interval);
+      return;
+    }
+
+    const elapsed = performance.now() - start;
+    timer.textContent = (elapsed / 1000).toFixed(3);
+  }, 1);
+}
